f684c84597d34277f49d16c5b82236edc57eef71
evanistyping <evansrodgers@gmail.com>
2022-06-06 19:39:19 -0700

    Header added.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+function ani(){
+    document.getElementById('button1').classList.toggle('spin1');
+    document.getElementById('button1').classList.toggle('spin2');
+}
+
+
+// const break1 = new Audio('../audio/break1.mp3');
+// const break2 = new Audio('../audio/break2.mp3');
+// const break3 = new Audio('../audio/break3.mp3');
+// const break4 = new Audio('../audio/break4.mp3');
+// const break5 = new Audio('../audio/break5.mp3');
+
+// function playSound(){
+//     console.log("SOUND");
+//     switch(Math.random() * 5){
+//         case 0: break1.play(); break;
+//         case 1: break2.play(); break;
+//         case 2: break3.play(); break;
+//         case 3: break4.play(); break;
+//         case 4: break5.play(); break;
+//         default: break;
+//     }
+// }
